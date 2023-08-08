d3351e36461573a24aa186d7a5a271280ba07575
Helena Nunes <lenanun3s@gmail.com>
2023-08-08 04:38:55 -0300

    feat(home): adiciona conteúdo

--- template-homepage.php
+++ template-homepage.php
@@ -3,9 +3,16 @@
 get_header();
 ?>
 
-<main class="main" role="main">
+<main class="main l-home" role="main">
 
     <h1 class="hide"><?= get_bloginfo('title') . ' - ' . get_bloginfo('description'); ?></h1>
+
+
+    <section class="l-home__content">
+        <div class="container">
+            <?php the_content();?>
+        </div>
+    </section>
 
 
 </main>
