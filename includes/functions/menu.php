9c43f4e47ad98faf70dd68263ca8a05e9f0c042c
Helena Nunes <lenanun3s@gmail.com>
2023-08-08 03:18:42 -0300

    feat(header): Adiciona versão desktop

--- includes/functions/menu.php
+++ includes/functions/menu.php
@@ -4,9 +4,25 @@
 function register_menu()
 {
 	register_nav_menus([
-		// Using array to specify more menus if needed
-		'header-menu' => 'Header Menu', // Main Navigation
-		'footer-menu' => 'Footer Menu', // Sidebar Navigation
+		'header_menu_desktop' => 'Header menu desktop',
+		'footer-menu' => 'Footer menu',
 	]);
 }
 add_action('init', 'register_menu');
+
+
+
+class Custom_Submenu_Walker extends Walker_Nav_Menu
+{
+	function start_lvl(&$output, $depth = 0, $args = null)
+	{
+		// Add a container div before the submenu starts
+		$output .= '<div class="submenu-container"><ul class="sub-menu">';
+	}
+
+	function end_lvl(&$output, $depth = 0, $args = null)
+	{
+		// Close the container div after the submenu ends
+		$output .= '</ul></div>';
+	}
+}
--- includes/modules/header.php
+++ includes/modules/header.php
@@ -5,7 +5,7 @@ global $theme_uri;
 <header class="m-header">
     
     <section class="m-header__main">
-        <div class="m-header__container">
+        <div class="container">
             <button class="m-button m-button-header JS__toggle-menu" aria-label="Open menu">
                 <span class="hamburger-inner"></span>
             </button>
@@ -14,19 +14,24 @@ global $theme_uri;
                 <img src="<?= $theme_uri; ?>/assets/img/logo.png" width="143" height="58" loading="lazy" alt="Intibiome - Co-Developed With Gynaecologists Logo">
             </a>
 
-            <button class="m-button --header" aria-label="Open search">
+            <button class="m-button m-button-header" aria-label="Open search">
                 <img src="<?= $theme_uri ?>/assets/img/icons/search.svg" width="24" height="24" loading="lazy" alt="Ícone de Lupa">
             </button>
         </div>
     </section>
 
     <section class="m-header__menu JS__header-menu">
-        
-        <div class="m-header__container">
+        <div class="container">
             <?php wp_nav_menu([
                 'menu'              => 'header',
-                'menu_class'        => 'm-header__menu__nav',
+                'menu_class'        => 'm-header__menu__nav mobile',
                 'container'       => 'nav'
+            ]); ?>
+            <?php wp_nav_menu([
+                'menu'              => 'header_menu_desktop',
+                'menu_class'        => 'm-header__menu__nav desktop',
+                'container'       => 'nav',
+                'walker' => new Custom_Submenu_Walker()
             ]); ?>
         </div>
     
